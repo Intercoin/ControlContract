64867e00d889c8a43454c2ef94b7766f9823c8f6
Artem Subbotin <subbotin.artem@gmail.com>
2024-05-30 23:07:28 +0300

    fixes in deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -44,7 +44,11 @@ module.exports = {
       url: bscURL,
       chainId: 56,
       gasPrice: "auto",
-      accounts: [process.env.private_key],
+      accounts: [
+        process.env.private_key,
+        process.env.private_key_auxiliary,
+        process.env.private_key_releasemanager,
+        ],
       saveDeployments: true
     },
     matic: {
